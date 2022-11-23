19e357837a1fd11d17762a1bb67e3f29d180f877
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-11-23 19:15:32 +0100

    F!! add restructuring data code

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
     packages=find_packages(include=['rattlinbog', 'rattlinbog.*']),
     install_requires=["xarray", "rioxarray", "rasterio", "eotransform", "eotransform-xarray", "eotransform-pandas",
                       "geopandas", "jupyter", "numba", "numpy", "matplotlib", "dask", "distributed", "geopathfinder",
-                      "equi7grid", "affine"],
-    extras_require={"test": ['pytest', 'pytest-cov']},
+                      "equi7grid", "affine", "zarr"],
+    extras_require={"test": ['pytest', 'pytest-cov'], "views": ['holoviews', 'datashader', 'panel', 'param']},
     include_package_data=True,
     zip_safe=False,
     python_requires='>=3.6'
