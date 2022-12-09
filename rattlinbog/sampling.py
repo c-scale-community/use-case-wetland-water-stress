c6bbadbfdcd23142c2f099bfadac5e8a25cbc290
Berhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-12-09 17:34:15 +0100

    F!! add histogram masking

--- rattlinbog/sampling.py
+++ rattlinbog/sampling.py
@@ -1,9 +1,11 @@
-from typing import Sequence, Optional
+from typing import Sequence, Optional, Union
 
 import numpy as np
 import xarray as xr
 from numpy.random import RandomState
 from xarray import Dataset, DataArray
+
+Scalars = Union[Sequence[float], Sequence[int]]
 
 
 def sample_uniformly(ds: Dataset, sample_masks: Sequence[DataArray], n: int, seed: Optional[int] = None) -> Dataset:
@@ -27,3 +29,16 @@ def make_quantile_masks(da: DataArray, quantiles: Sequence[float]) -> Sequence[D
     return [DataArray(np.logical_and(da.values > qs[i], da.values <= qs[i + 1]),
                       dims=da.dims, coords=da.coords, name=f"quantile_{quantiles[i]}")
             for i in range(len(qs) - 1)]
+
+
+def make_histogram_masks(da: DataArray, bins: Union[int, Scalars, str]) -> Sequence[DataArray]:
+    def make_mask(lower, upper, is_last):
+        return DataArray(np.logical_and(da.values >= lower,
+                                        # all but the right-most are half open - see note:
+                                        # https://numpy.org/doc/stable/reference/generated/numpy.histogram.html
+                                        da.values <= upper if is_last else da.values < upper),
+                         dims=da.dims, coords=da.coords,
+                         name=f"bin_[{lower}, {upper}{']' if is_last else ')'}")
+
+    edges = np.histogram_bin_edges(da.values.flatten(), bins=bins)
+    return [make_mask(edges[i], edges[i + 1], i == len(edges) - 2) for i in range(len(edges) - 1)]
--- tests/test_mask_generation.py
+++ tests/test_mask_generation.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from xarray import DataArray
 
-from rattlinbog.sampling import make_quantile_masks
+from rattlinbog.sampling import make_quantile_masks, make_histogram_masks
 
 
 @pytest.fixture
@@ -18,7 +18,6 @@ def balanced_da():
 
 def test_make_quantile_masks(balanced_da):
     masks = make_quantile_masks(balanced_da, quantiles=[0, 0.25, 0.5, 0.75, 1.0])
-
     np.testing.assert_equal(masks[0].values, true_at_row(0))
     np.testing.assert_equal(masks[1].values, true_at_row(1))
     np.testing.assert_equal(masks[2].values, true_at_row(2))
@@ -36,7 +35,28 @@ def test_quantile_masks_upper_bound_is_greater_or_equal(balanced_da):
     np.testing.assert_equal(masks[0].values, true_at_row(slice(0, 3)))
     np.testing.assert_equal(masks[1].values, true_at_row(slice(3, 5)))
 
+
 def test_quantile_masks_come_with_a_name(balanced_da):
     masks = make_quantile_masks(balanced_da, quantiles=[0.5, 1.0])
     assert masks[0].name == "quantile_0.5"
     assert masks[1].name == "quantile_1.0"
+
+
+def test_histogram_mask(balanced_da):
+    masks = make_histogram_masks(balanced_da, bins=5)
+    np.testing.assert_equal(masks[0].values, true_at_row(0))
+    np.testing.assert_equal(masks[1].values, true_at_row(1))
+    np.testing.assert_equal(masks[2].values, true_at_row(2))
+    np.testing.assert_equal(masks[3].values, true_at_row(3))
+
+
+def test_histogram_mask_with_bins_as_a_sequence(balanced_da):
+    masks = make_histogram_masks(balanced_da, bins=[-2, 0, 2])
+    np.testing.assert_equal(masks[0].values, true_at_row(slice(0, 2)))
+    np.testing.assert_equal(masks[1].values, true_at_row(slice(2, 5)))
+
+def test_histogram_masks_come_with_a_name(balanced_da):
+    masks = make_histogram_masks(balanced_da, bins=[-2, -1, 1, 2])
+    assert masks[0].name == "bin_[-2, -1)"
+    assert masks[1].name == "bin_[-1, 1)"
+    assert masks[2].name == "bin_[1, 2]"
