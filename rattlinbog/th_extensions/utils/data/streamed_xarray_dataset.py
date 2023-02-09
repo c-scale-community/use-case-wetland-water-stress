758e5189a49cc96f38c23183e9235bf57f2711ad
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-02-09 10:55:35 +0100

    F!! create dask graph lazily

--- rattlinbog/th_extensions/utils/data/streamed_xarray_dataset.py
+++ rattlinbog/th_extensions/utils/data/streamed_xarray_dataset.py
@@ -10,7 +10,7 @@ class StreamedXArrayDataset(IterableDataset):
     def __init__(self, xarray_source: Iterable[Dataset],
                  input_label_splitter: Callable[[Dataset], Tuple[NDArray, NDArray]],
                  estimated_len: Optional[int] = None):
-        self._delayed_splits = [dask.delayed(input_label_splitter)(d) for d in xarray_source]
+        self._delayed_splits = map(dask.delayed(input_label_splitter), xarray_source)
         self.estimated_len = estimated_len
 
     def __iter__(self) -> Iterator[Tuple[NDArray, NDArray]]:
