fc2bd8a928c14251477bda32c36f5bb11ef8f9f3
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-02-09 21:43:54 +0100

    F!! allow multiple classes by properly specifying chunks

--- tests/test_apply_estimator_to_data_array.py
+++ tests/test_apply_estimator_to_data_array.py
@@ -1,12 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Iterable, Sequence
+from typing import Sequence
 
-import dask as da
 import numpy as np
 import pytest
 import xarray as xr
 from numpy._typing import NDArray
-from sklearn.base import BaseEstimator
 from xarray import DataArray
 
 from factories import make_raster
@@ -31,9 +29,11 @@ class _DataArrayMapper:
         self._estimator = estimator
 
     def to(self, array: DataArray) -> DataArray:
-        out_template = array[0, :, :].drop_vars(array.dims[0]).expand_dims({'classes': self._estimator.classes})
+        classes = self._estimator.classes
+        c_sizes = array.chunksizes
+        out_template = array[0, :, :].drop_vars(array.dims[0]).expand_dims({'classes': classes})
         estimated = array.data.map_blocks(lambda block: self._estimator.predict(block), drop_axis=0, new_axis=0,
-                                          meta=out_template.data)
+                                          chunks=(len(classes), c_sizes['y'], c_sizes['x']), meta=out_template.data)
         return out_template.copy(data=estimated)
 
 
@@ -62,6 +62,23 @@ def estimate_always_true():
     return AlwaysTrue()
 
 
+# turn of inspections that collide with scikit-learn API requirements & style guide, see:
+# https://scikit-learn.org/stable/developers/develop.html
+# noinspection PyPep8Naming,PyAttributeOutsideInit
+class MultiClassEstimator(ClassEstimator):
+    def predict(self, X: NDArray) -> NDArray:
+        return np.full((4,) + X.shape[1:], 0.25)
+
+    @property
+    def classes(self) -> Sequence[str]:
+        return ["a", "b", "c", "d"]
+
+
+@pytest.fixture
+def estimate_multiple_classes():
+    return MultiClassEstimator()
+
+
 def test_applying_classification_estimator_to_data_array_chunks(estimate_always_true):
     estimated = apply_classification(estimate_always_true).to(make_raster(np.zeros((2, 4, 4))).chunk(2))
     assert_arrays_identical(estimated.load(), make_raster(np.ones((1, 4, 4)),
@@ -75,3 +92,9 @@ def assert_arrays_identical(actual, expected):
 def test_estimator_is_called_once_per_chunk(estimate_always_true):
     apply_classification(estimate_always_true).to(make_raster(np.zeros((2, 4, 4))).chunk(2)).compute()
     assert estimate_always_true.num_predictions == 4
+
+
+def test_estimate_multiple_classes(estimate_multiple_classes):
+    estimated = apply_classification(estimate_multiple_classes).to(make_raster(np.zeros((2, 4, 4))).chunk(2)).compute()
+    assert_arrays_identical(estimated.load(), make_raster(np.full((4, 4, 4), 0.25),
+                                                          param_dim=('classes', estimate_multiple_classes.classes)))
