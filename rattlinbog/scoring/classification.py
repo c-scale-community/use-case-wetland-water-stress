5c360cd853fbefe2c95514a36805e74b0dc71d5a
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-02-13 17:48:32 +0100

    R! extract zeroth order scoring

--- rattlinbog/scoring/classification.py
+++ rattlinbog/scoring/classification.py
@@ -1,7 +1,29 @@
 from dataclasses import dataclass
 
 import numpy as np
-from numpy._typing import NDArray
+from numpy.typing import NDArray
+
+
+@dataclass
+class ClassScore0th:
+    TP: NDArray
+    TN: NDArray
+    FP: NDArray
+    FN: NDArray
+
+    def __eq__(self, other: "ClassScore0th") -> bool:
+        return np.array_equal(self.TP, other.TP) \
+            and np.array_equal(self.TN, other.TN) \
+            and np.array_equal(self.FP, other.FP) \
+            and np.array_equal(self.FN, other.FN)
+
+
+def score_zero_order(confusion_matrix: NDArray) -> ClassScore0th:
+    tp = np.diag(confusion_matrix)
+    fp = confusion_matrix.sum(axis=0) - tp
+    fn = confusion_matrix.sum(axis=1) - tp
+    tn = confusion_matrix.sum() - (fp + fn + tp)
+    return ClassScore0th(tp, tn, fp, fn)
 
 
 @dataclass
@@ -16,27 +38,41 @@ class ClassScore1st:
             and np.array_equal(self.PPV, other.PPV)
 
 
-def score_confusion_matrix_first_order(confusion_matrix: NDArray) -> ClassScore1st:
-    tp = np.diag(confusion_matrix)
-    fp = confusion_matrix.sum(axis=0) - tp
-    fn = confusion_matrix.sum(axis=1) - tp
-    tn = confusion_matrix.sum() - (fp + fn + tp)
-
-    p = tp + fn
-    n = tn + fp
+def score_first_order(zero_order: ClassScore0th) -> ClassScore1st:
+    p = zero_order.TP + zero_order.FN
+    n = zero_order.TN + zero_order.FP
 
     has_p = p > 0
     has_not_p = ~has_p
-    tpr = np.empty_like(tp, dtype=np.float32)
-    tpr[has_p] = tp[has_p] / p[has_p]
-    tpr[has_not_p] = 1.0 - tn[has_not_p] / n[has_not_p]
+    tpr = np.empty_like(zero_order.TP, dtype=np.float32)
+    tpr[has_p] = zero_order.TP[has_p] / p[has_p]
+    tpr[has_not_p] = 1.0 - zero_order.TN[has_not_p] / n[has_not_p]
 
     has_n = n > 0
     has_not_n = ~has_n
-    tnr = np.empty_like(tp, dtype=np.float32)
-    tnr[has_n] = tn[has_n] / n[has_n]
-    tnr[has_not_n] = 1 - tp[has_not_n] / p[has_not_n]
+    tnr = np.empty_like(zero_order.TP, dtype=np.float32)
+    tnr[has_n] = zero_order.TN[has_n] / n[has_n]
+    tnr[has_not_n] = 1 - zero_order.TP[has_not_n] / p[has_not_n]
 
-    ppv = tp / (tp + fp)
+    ppv = zero_order.TP / (zero_order.TP + zero_order.FP)
 
     return ClassScore1st(tpr, tnr, ppv)
+
+
+# @dataclass
+# class ClassScore2nd:
+#     F1: NDArray
+#     BA: NDArray
+#     MCC: NDArray
+#
+#     def __eq__(self, other: "ClassScore2nd") -> bool:
+#         return np.array_equal(self.F1, other.F1) \
+#             and np.array_equal(self.BA, other.BA) \
+#             and np.array_equal(self.MCC, other.MCC)
+#
+#
+# def score_confusion_matrix_second_order(confusion_matrix: NDArray, first_order: ClassScore1st) -> ClassScore2nd:
+#     f1 = 2.0 * (first_order.PPV * first_order.TPR) / (first_order.PPV + first_order.TPR)
+#     ba = (first_order.TPR + first_order.TNR) / 2.0
+#     mcc = (())
+#     return ClassScore2nd()
--- tests/test_classification_scores.py
+++ tests/test_classification_scores.py
@@ -3,7 +3,8 @@ import pytest
 from numpy.typing import NDArray
 from sklearn.metrics import confusion_matrix
 
-from rattlinbog.scoring.classification import ClassScore1st, score_confusion_matrix_first_order
+from rattlinbog.scoring.classification import ClassScore0th, ClassScore1st, score_zero_order, \
+    score_first_order
 
 
 def make_mask(description):
@@ -17,17 +18,29 @@ def as_a(*values) -> NDArray:
 
 TEST_SETUP = {
     "perfect true positives": (make_mask("1111"), make_mask("1111"),
+                               ClassScore0th(TP=as_a(4), TN=as_a(0), FP=as_a(0), FN=as_a(0)),
                                ClassScore1st(TPR=as_a(1.0), TNR=as_a(0.0), PPV=as_a(1.0))),
+    # ClassScore2nd(F1=as_a(1.0), BA=as_a(0.0), MCC=as_a(1.0))),
     "perfect false positives": (make_mask("0000"), make_mask("0000"),
+                                ClassScore0th(TP=as_a(4), TN=as_a(0), FP=as_a(0), FN=as_a(0)),
                                 ClassScore1st(TPR=as_a(1.0), TNR=as_a(0.0), PPV=as_a(1.0))),
+    # ClassScore2nd(F1=as_a(1.0), BA=as_a(0.0), MCC=as_a(1.0))),
     "perfectly correct": (make_mask("0011"), make_mask("0011"),
+                          ClassScore0th(TP=as_a(2, 2), TN=as_a(2, 2), FP=as_a(0, 0), FN=as_a(0, 0)),
                           ClassScore1st(TPR=as_a(1.0, 1.0), TNR=as_a(1.0, 1.0), PPV=as_a(1.0, 1.0))),
+    # ClassScore2nd(F1=as_a(1.0), BA=as_a(0.0), MCC=as_a(1.0))),
     "perfectly incorrect": (make_mask("0011"), make_mask("1100"),
+                            ClassScore0th(TP=as_a(0, 0), TN=as_a(0, 0), FP=as_a(2, 2), FN=as_a(2, 2)),
                             ClassScore1st(TPR=as_a(0.0, 0.0), TNR=as_a(0.0, 0.0), PPV=as_a(0.0, 0.0))),
+    # ClassScore2nd(F1=as_a(1.0), BA=as_a(0.0), MCC=as_a(1.0))),
     "mostly correct": (make_mask("0011"), make_mask("0111"),
+                       ClassScore0th(TP=as_a(1, 2), TN=as_a(2, 1), FP=as_a(0, 1), FN=as_a(1, 0)),
                        ClassScore1st(TPR=as_a(0.5, 1.0), TNR=as_a(1.0, 0.5), PPV=as_a(1.0, 2 / 3))),
+    # ClassScore2nd(F1=as_a(1.0), BA=as_a(0.0), MCC=as_a(1.0))),
     "mostly incorrect": (make_mask("1111"), make_mask("0001"),
+                         ClassScore0th(TP=as_a(0, 1), TN=as_a(1, 0), FP=as_a(3, 0), FN=as_a(0, 3)),
                          ClassScore1st(TPR=as_a(0.75, 0.25), TNR=as_a(0.25, 0.75), PPV=as_a(0.0, 1.0))),
+    # ClassScore2nd(F1=as_a(1.0), BA=as_a(0.0), MCC=as_a(1.0))),
 }
 
 
@@ -43,9 +56,26 @@ def cm(active_setup):
 
 
 @pytest.fixture
-def expected_score(active_setup):
+def expected_score_0th(active_setup):
     return TEST_SETUP[active_setup][2]
 
 
-def test_calculating_first_order_scores(cm, expected_score):
-    assert score_confusion_matrix_first_order(cm) == expected_score
+@pytest.fixture
+def expected_score_1st(active_setup):
+    return TEST_SETUP[active_setup][3]
+
+
+# @pytest.fixture
+# def expected_score_2nd(active_setup):
+#     return TEST_SETUP[active_setup][3]
+
+
+def test_calculating_zero_order_scores(cm, expected_score_0th):
+    assert score_zero_order(cm) == expected_score_0th
+
+
+def test_calculating_first_order_scores(expected_score_0th, expected_score_1st):
+    assert score_first_order(expected_score_0th) == expected_score_1st
+
+# def test_calculating_second_order_scores(cm, expected_score_1st, expected_score_2nd):
+#     assert score_confusion_matrix_second_order(cm, expected_score_1st) == expected_score_2nd
