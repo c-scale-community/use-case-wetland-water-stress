caddb872e245c85839199c8d3554351e2be08150
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-02-13 19:19:54 +0100

    F!! add FPR and FNR to first order scores
    ... and fix wrong handling of edge cases

--- rattlinbog/scoring/classification.py
+++ rattlinbog/scoring/classification.py
@@ -30,6 +30,8 @@ def score_zero_order(confusion_matrix: NDArray) -> ClassScore0th:
 class ClassScore1st:
     TPR: NDArray
     TNR: NDArray
+    FPR: NDArray
+    FNR: NDArray
     PPV: NDArray
 
     def __eq__(self, other: "ClassScore1st") -> bool:
@@ -42,25 +44,26 @@ def score_first_order(zero_order: ClassScore0th) -> ClassScore1st:
     p = zero_order.TP + zero_order.FN
     n = zero_order.TN + zero_order.FP
 
-    has_p = p > 0
-    has_not_p = ~has_p
-    tpr = np.empty_like(zero_order.TP, dtype=np.float32)
-    tpr[has_p] = zero_order.TP[has_p] / p[has_p]
-    tpr[has_not_p] = 1.0 - zero_order.TN[has_not_p] / n[has_not_p]
+    tpr = zero_order.TP / p
+    tnr = zero_order.TN / n
+    fpr = zero_order.FP / n
+    fnr = zero_order.FN / p
 
-    has_n = n > 0
-    has_not_n = ~has_n
-    tnr = np.empty_like(zero_order.TN, dtype=np.float32)
-    tnr[has_n] = zero_order.TN[has_n] / n[has_n]
-    tnr[has_not_n] = 1 - zero_order.TP[has_not_n] / p[has_not_n]
+    no_p = p == 0
+    no_n = n == 0
+
+    tpr[no_p] = 0.0
+    fnr[no_p] = 0.0
+    tnr[no_n] = 0.0
+    fpr[no_n] = 0.0
+
 
     t_and_f_ps = zero_order.TP + zero_order.FP
-    has_ppv = t_and_f_ps > 0
-    ppv = np.empty_like(zero_order.TP, dtype=np.float32)
-    ppv[has_ppv] = zero_order.TP[has_ppv] / t_and_f_ps[has_ppv]
-    ppv[~has_ppv] = 0.0
+    no_ppv = t_and_f_ps == 0
+    ppv = zero_order.TP / t_and_f_ps
+    ppv[no_ppv] = 0.0
 
-    return ClassScore1st(tpr, tnr, ppv)
+    return ClassScore1st(tpr, tnr, fpr, fnr, ppv)
 
 
 @dataclass
@@ -74,10 +77,11 @@ class ClassScore2nd:
 
 
 def score_second_order(first_order: ClassScore1st) -> ClassScore2nd:
-    f1 = np.empty_like(first_order.TPR)
     precision_and_rate = first_order.PPV + first_order.TPR
-    is_valid = precision_and_rate > 0
-    f1[is_valid] = 2.0 * (first_order.PPV[is_valid] * first_order.TPR[is_valid]) / precision_and_rate[is_valid]
-    f1[~is_valid] = 0.0
+    f1 = 2.0 * (first_order.PPV * first_order.TPR) / precision_and_rate
+
+    no_f1 = precision_and_rate == 0
+    f1[no_f1] = 0.0
+
     ba = (first_order.TPR + first_order.TNR) / 2.0
     return ClassScore2nd(f1, ba)
