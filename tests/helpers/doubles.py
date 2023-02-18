F!! fix loss calculation

tests/helpers/doubles.py
@@ -1,5 +1,6 @@
 import time
 from collections import defaultdict
+from typing import Optional
 
 import numpy as np
 import torch as th
@@ -14,9 +15,11 @@ from rattlinbog.th_extensions.utils.dataset_splitters import split_to_params_and
 class AlwaysTrue(Estimator):
     def __init__(self):
         self.num_predictions = 0
+        self.received_param = None
 
-    def predict(self, X: NDArray) -> NDArray:
+    def predict(self, X: NDArray, param: Optional[str] = None) -> NDArray:
         self.num_predictions += 1
+        self.received_param = param
         return np.ones((1,) + X.shape[1:])
 
     def score(self, X: NDArray, y: NDArray) -> Score:
@@ -111,15 +114,21 @@ class DelayingSplit:
 
 class ScoreableEstimatorSpy(ScoreableEstimator):
     def __init__(self):
-        self.returned_estimate = np.zeros((1, 32, 32))
+        self.returned_raw_estimate = np.zeros((1, 32, 32))
+        self.returned_refined_estimate = np.ones((1, 32, 32))
         self.returned_loss = 0.042
         self.returned_score = {'A': 42, 'B': 0.42}
+        self.loss_received = None
         self.scorer_received = None
 
-    def predict(self, X: NDArray) -> NDArray:
-        return self.returned_estimate
+    def predict(self, X: NDArray, **kwargs) -> NDArray:
+        return self.returned_raw_estimate
+
+    def refine_raw_estimate(self, estimate: NDArray) -> NDArray:
+        return self.returned_refined_estimate
 
     def loss_for_estimate(self, estimate: NDArray, ground_truth: NDArray) -> float:
+        self.loss_received = (estimate, ground_truth)
         return self.returned_loss
 
     def score_estimate(self, estimate: NDArray, ground_truth: NDArray) -> Score: