2c8fd9e53a402dd6419acafbd7f5d71649cd55e4
Berhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-05-08 15:57:18 +0200

    F!! implemented indices data concatenation

--- tests/test_mosaice_train_and_sample_data.py
+++ tests/test_mosaice_train_and_sample_data.py
@@ -4,8 +4,9 @@ from typing import Tuple
 import numpy as np
 import pytest
 import xarray as xr
-from xarray import Dataset
+from xarray import Dataset, DataArray
 
+from rattlinbog.sampling.sample_patches_from_dataset import SamplingConfig, make_balanced_sample_indices_for
 from rattlinbog.th_extensions.utils.dataset_splitters import PARAMS_KEY, GROUND_TRUTH_KEY
 from tests.helpers.factories import make_raster
 
@@ -43,12 +44,12 @@ def make_train_dataset(ys, xs):
     })
 
 
-def concatenate_training_dataset(*zarrs: Tuple[Path, ...]) -> Dataset:
+def concatenate_training_datasets(*zarrs: Tuple[Path, ...]) -> Dataset:
     return xr.open_mfdataset(zarrs, engine='zarr', combine='by_coords', chunks={})
 
 
 def test_concatenate_two_training_datasets(train_west, train_east):
-    train = concatenate_training_dataset(train_west, train_east)
+    train = concatenate_training_datasets(train_west, train_east)
     assert_coordinates(train.x.values, np.arange(0, 200))
     assert_coordinates(train.y.values, np.arange(100, 0, -1))
 
@@ -58,12 +59,49 @@ def assert_coordinates(actual_coords, expected_coords):
 
 
 def test_concatenate_non_neighbouring_training_datasets(train_west, train_offset_by_one_east):
-    train = concatenate_training_dataset(train_west, train_offset_by_one_east)
+    train = concatenate_training_datasets(train_west, train_offset_by_one_east)
     assert_coordinates(train.x.values, np.concatenate([np.arange(0, 100), np.arange(200, 300)]))
     assert_coordinates(train.y.values, np.arange(100, 0, -1))
 
 
 def test_selecting_from_offset_produces_zero_size_patch(train_west, train_offset_by_one_east):
-    train = concatenate_training_dataset(train_west, train_offset_by_one_east)
+    train = concatenate_training_datasets(train_west, train_offset_by_one_east)
     out_of_bounds = train.sel(y=slice(30, 40), x=slice(150, 160))
     assert out_of_bounds.dims['y'] == 0 and out_of_bounds.dims['x'] == 0
+
+
+def concatenate_indices_dataset(*zarrs: Tuple[Path, ...]) -> DataArray:
+    return xr.open_mfdataset(zarrs, engine='zarr', concat_dim='pos', combine='nested', chunks={}).to_array()
+
+
+
+@pytest.fixture
+def indices_west():
+    ds = make_train_dataset(np.arange(100, 0, -1), np.arange(0, 100))
+    return make_balanced_sample_indices_for(ds, SamplingConfig(8, 16))
+
+
+@pytest.fixture
+def indices_west_zarr(tmp_path, indices_west):
+    zarr = tmp_path / "indices_west.zarr"
+    indices_west.to_zarr(zarr)
+    return zarr
+
+
+@pytest.fixture
+def indices_east():
+    ds = make_train_dataset(np.arange(100, 0, -1), np.arange(100, 200))
+    return make_balanced_sample_indices_for(ds, SamplingConfig(8, 16))
+
+
+@pytest.fixture
+def indices_east_zarr(tmp_path, indices_east):
+    zarr = tmp_path / "indices_east.zarr"
+    indices_east.to_zarr(zarr)
+    return zarr
+
+
+def test_concatenate_two_indices_dataset(indices_west_zarr, indices_east_zarr, indices_west, indices_east):
+    indices = concatenate_indices_dataset(indices_west_zarr, indices_east_zarr)
+    assert_coordinates(indices.x.values, np.concatenate([indices_west.x.values, indices_east.x.values]))
+    assert_coordinates(indices.y.values, np.concatenate([indices_west.y.values, indices_east.y.values]))
