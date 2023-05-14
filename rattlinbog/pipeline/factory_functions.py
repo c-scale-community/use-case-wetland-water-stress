3d2f7ab584b19fca4881b0a75a26190ae7c225ce
Berhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-05-14 17:57:16 +0200

    F!! add model sink to log cfg factory

--- rattlinbog/pipeline/factory_functions.py
+++ rattlinbog/pipeline/factory_functions.py
@@ -2,6 +2,6 @@ from rattlinbog.estimators.base import ValidationLogging, LogConfig
 from rattlinbog.evaluate.validation_source_from_dataset import ValidationSourceFromDataset
 
 
-def make_validation_log_cfg(valid_ds, train_log, valid_log, score_freq, image_freq):
+def make_validation_log_cfg(valid_ds, train_log, valid_log, score_freq, image_freq, model_sink):
     return LogConfig(train_log, ValidationLogging(valid_log, ValidationSourceFromDataset(valid_ds),
-                                                  score_freq, image_freq))
+                                                  score_freq, image_freq, model_sink))
